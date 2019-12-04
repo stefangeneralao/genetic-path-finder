79752308d3334f154366c5caf38c158a52f996d3
stefangeneralao <stefangeneralao@gmail.com>
2019-12-04 04:50:26 +0000

    Fix: Execution cleaned too much

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -5,7 +5,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 yarn
 docker build -t genetic_path_finder .
-rm -rf build
 docker stop genetic_path_finder || true
 docker rm genetic_path_finder || true
 docker run -p 3003:3003 -d --name genetic_path_finder genetic_path_finder
